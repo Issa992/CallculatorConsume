2ca57d17a931a0ab21d79f6fc939b9290f6014f8
Issa <abdu2997@edu.easj.dk>
2018-10-02 15:07:56 +0200

    CalculatorConsume

---
+++ CallculatorConsume/Data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallculatorConsume
+{
+   public class Data
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        public Data(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public Data()
+        {
+
+        }
+    }
+}
--- CallculatorConsume/Program.cs
+++ CallculatorConsume/Program.cs
@@ -1,21 +1,55 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 
 namespace CallculatorConsume
 {
     class Program
     {
+        private static string CalculatorUri = "https://localhost:44319/api/values/Add";
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
+
+            RunAsync();
+
             Console.ReadKey();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+
         }
+
+      public static async Task RunAsync()
+        {
+         
+              await  AsyncAdd(new Data(1, 5));
+       
+        }
+
+        public static async Task<String> AsyncAdd(Data data)
+        {
+            using (HttpClient client=new HttpClient())
+            {
+                Console.WriteLine("Data " + data);
+                var jsonString = JsonConvert.SerializeObject(data);
+                Console.WriteLine("json string: " + jsonString);
+                StringContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                Console.WriteLine("content: : " + content.ToString());
+                Console.WriteLine("CalculatorUri: " + CalculatorUri);
+
+                HttpResponseMessage response = await client.PostAsync(CalculatorUri , content);
+                string str = await response.Content.ReadAsStringAsync();
+                //Int32 sumStr = JsonConvert.DeserializeObject<Int32>(str);
+                Console.WriteLine("Result is "+str);
+                return str;
+
+
+            }
+
+        }
+
     }
 }
